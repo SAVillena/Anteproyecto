79b0eefb0dbe2906e413e3cc8f0dc5de274b7b0b
Patricio Villalon <patricio.villalon2001@alumnos.ubiobio.cl>
2024-11-09 22:26:02 -0300

    Validacion de RUT Frontend
    Agregado la Libreria 'validar-rut'.
    Modificacion del campo RUT en la creacion de usuarios.
    Se torna roja el campo cuando esta erroneo y a traves de un snackbar con Alerta se muestra una notificacion en la esquina superior derecha.

--- frontend/src/components/UserManagement/UserForm.jsx
+++ frontend/src/components/UserManagement/UserForm.jsx
@@ -1,12 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import {
-  Dialog, DialogTitle, DialogContent, Grid, TextField, Button, Select, MenuItem, FormControl, InputLabel, Box,
+  Dialog, DialogTitle, DialogContent, Grid, TextField, Button, Select, MenuItem, FormControl, InputLabel, Box, Snackbar,
+  Alert,
 } from '@mui/material';
+import { validateRUT } from 'validar-rut';
 
 const UserForm = ({ user, onClose, onSubmit }) => {
   const [formData, setFormData] = useState({
     username: '', email: '', rut: '', password: '', newPassword: '', roles: ['user'],
   });
+  const [rutError, setRutError] = useState(false);
+  const [openSnackbar, setOpenSnackbar] = useState(false);
 
   useEffect(() => {
     if (user) {
@@ -28,14 +32,47 @@ const UserForm = ({ user, onClose, onSubmit }) => {
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: name === 'roles' ? [value] : value });
+    if (name === 'rut') {
+      const formattedRut = formatearRUT(value);
+      setFormData({ ...formData, rut: formattedRut });
+    } else {
+      setFormData({ ...formData, [name]: name === 'roles' ? [value] : value });
+    }
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    if (!validateRUT(formData.rut) || !validarFormatoRUT(formData.rut)) {
+      setRutError(true);
+      setOpenSnackbar(true);
+      return;
+    } else {
+      setRutError(false);
+    }
     const dataToSend = { ...formData };
     if (!user) delete dataToSend.newPassword;
     else if (!dataToSend.newPassword) delete dataToSend.newPassword;
     onSubmit(dataToSend);
+  };
+
+  const validarFormatoRUT = (rut) => {
+    const regex = /^[0-9]{1,2}\.[0-9]{3}\.[0-9]{3}-[0-9K]$/;
+    return regex.test(rut);
+  };
+
+  const formatearRUT = (rut) => {
+    let rutLimpio = rut.replace(/[^0-9kK]/g, '').toUpperCase();
+    if (rutLimpio.length > 9) {
+      rutLimpio = rutLimpio.slice(0, 9);
+    }
+    const cuerpo = rutLimpio.slice(0, -1);
+    const dv = rutLimpio.slice(-1);
+    const cuerpoFormateado = cuerpo.replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+    return `${cuerpoFormateado}-${dv}`;
+  };
+
+  const handleCloseSnackbar = () => {
+    setOpenSnackbar(false);
   };
 
   return (
@@ -66,13 +103,30 @@ const UserForm = ({ user, onClose, onSubmit }) => {
               />
             </Grid>
             <Grid item xs={12} sm={6}>
-              <TextField
+            <TextField
                 label="RUT"
                 name="rut"
                 value={formData.rut}
                 onChange={handleInputChange}
+                error={rutError}
                 fullWidth
                 required
+                sx={{
+                  '& .MuiOutlinedInput-root': {
+                    '& fieldset': {
+                      borderColor: rutError ? 'red' : 'default',
+                    },
+                    '&:hover fieldset': {
+                      borderColor: rutError ? 'red' : 'default',
+                    },
+                    '&.Mui-focused fieldset': {
+                      borderColor: rutError ? 'red' : 'default',
+                    },
+                  },
+                  '& .MuiInputLabel-root': {
+                    color: rutError ? 'red' : 'default',
+                  },
+                }}
               />
             </Grid>
             {!user && (
@@ -128,6 +182,15 @@ const UserForm = ({ user, onClose, onSubmit }) => {
           </Grid>
         </Box>
       </DialogContent>
+      <Snackbar
+        open={openSnackbar}
+        autoHideDuration={6000}
+        onClose={handleCloseSnackbar}
+        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}>
+        <Alert onClose={handleCloseSnackbar} severity="error" sx={{ width: '100%' }}>
+        RUT inválido.
+        </Alert>
+        </Snackbar>
     </Dialog>
   );
 };
